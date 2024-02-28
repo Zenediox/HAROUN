088cb041becc05b903c1f6e0f3b35864362efeb5
Zenediox <161477937+Zenediox@users.noreply.github.com>
2024-02-28 19:28:39 +0300

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
+  ['+255 626 252 258', 'haroun', true],
   ['6281255369012', 'Ash', true], 
   [''] 
 ] //Number of owners
